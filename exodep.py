645015612761cc73248d11582c81cd447aab963f
Pete Cordell <pete.git@codalogic.com>
2016-07-20 10:35:33 +0100

    Exodep update

--- exodep.py
+++ exodep.py
@@ -43,7 +43,11 @@ def main() :
     ProcessDeps( sys.argv[1] if len( sys.argv ) >= 2 else "mydeps.exodep" )
 
 class ProcessDeps:
+    are_any_files_changed = False
+
     def __init__( self, dependencies_src, vars = { 'strand': 'master', 'path': '' } ):
+        self.are_files_changed = False
+        self.line_num = 0
         self.uritemplate = host_templates['github']
         self.vars = vars.copy()
         self.versions = {}  # Each entry is <string of space separated strand names> : <string to use as strand in uri template>
@@ -56,7 +60,7 @@ class ProcessDeps:
             self.file = "<StringIO>"
             self.process_dependency_stream( dependencies_src )
         else:
-            print( "Error: Unrecognised dependencies_src type format" )
+            self.error( "Unrecognised dependencies_src type format" )
 
     processed_dependencies = {}
 
@@ -72,10 +76,11 @@ class ProcessDeps:
             with open(self.file) as f:
                 self.process_dependency_stream( f )
         except FileNotFoundError:
-            print( "Error: Unable to open exodep file:", self.file )
+            self.error( "Unable to open exodep file: " + self.file )
 
     def process_dependency_stream( self, f ):
         for line in f:
+            self.line_num += 1
             self.process_line( line )
 
     def process_line( self, line ):
@@ -84,19 +89,39 @@ class ProcessDeps:
         if is_blank_line( line ):
             return
         if not (self.consider_include( line ) or
+                self.consider_sinclude( line ) or
                 self.consider_hosting( line ) or
                 self.consider_uri_template( line ) or
                 self.consider_versions( line ) or
                 self.consider_variable( line ) or
                 self.consider_default_variable( line ) or
                 self.consider_copy( line ) or
-                self.consider_bcopy( line ) ):
+                self.consider_bcopy( line ) or
+                self.consider_file_ops( line ) or
+                self.consider_exec( line ) or
+                self.consider_subst( line ) or
+                self.consider_on_conditional( line ) or
+                self.consider_onchanged( line ) or
+                self.consider_onanychanged( line ) or
+                self.consider_os_conditional( line ) ):
             self.report_unrecognised_command( line )
 
     def consider_include( self, line ):
-        m = re.match( 'include\s+(.*)', line )
-        if m != None and m.group(1) != '':
-            ProcessDeps( self.script_relative_path( m.group(1) ), self.vars )
+        m = re.match( 'include\s+(.+)', line )
+        if m != None:
+            file_name = self.script_relative_path( m.group(1) )
+            if not os.path.isfile( file_name ):
+                self.error( "'include' file not found: " + file_name )
+                return True     # It was an 'include' command, even though it was a bad one
+            ProcessDeps( file_name, self.vars )
+            return True
+        return False
+
+    def consider_sinclude( self, line ):        # This method is used to support testing
+        m = re.match( 'sinclude\s+(.+)', line )
+        if m != None:
+            args = m.group(1)
+            ProcessDeps( io.StringIO( args.replace( '\t', '\n' ) ), self.vars )
             return True
         return False
 
@@ -104,18 +129,18 @@ class ProcessDeps:
         return os.path.normpath( os.path.join( os.path.dirname( self.file ), src ) ).replace( '\\', '/' )
 
     def consider_hosting( self, line ):
-        m = re.match( 'hosting\s+(.*)', line )
-        if m != None and m.group(1) != '':
+        m = re.match( 'hosting\s+(.+)', line )
+        if m != None:
             host = m.group(1)
             if host in host_templates:
                 self.uritemplate = host_templates[host]
             else:
-                print( "Error: Unrecognised hosting server provider:", host )
+                self.error( "Unrecognised hosting server provider: " + host )
             return True
         return False
 
     def consider_uri_template( self, line ):
-        m = re.match( 'uritemplate\s+(.*)', line )
+        m = re.match( 'uritemplate\s+(.+)', line )
         if m != None:
             self.uritemplate = m.group(1)
             return True
@@ -134,7 +159,7 @@ class ProcessDeps:
                     with open( uri, "rt" ) as fin:
                         self.parse_versions_info( fin )
             except:
-                print( "Error: Unable to retrieve:", uri )
+                self.error( "Unable to retrieve: " + uri )
             return True
         return False
 
@@ -179,7 +204,7 @@ class ProcessDeps:
         return False
 
     def report_unrecognised_command( self, line ):
-        print( "Error: Unrecognised command:", line )
+        self.error( "Unrecognised command: " + line )
 
     def retrieve_text_file( self, src, dst ):
         self.retrieve_file( src, dst, TextDownloadHandler() )
@@ -190,7 +215,7 @@ class ProcessDeps:
     def retrieve_file( self, src, dst, handler ):
         if dst == None:
             if re.match( 'https?://', src ):
-                print( "Error: Explicit uri not supported with commands of the form 'bcopy src_and_dst'" )
+                self.error( "Explicit uri not supported with commands of the form 'bcopy src_and_dst'" )
                 return
             dst = src
             if re.search( '\$\{path\}', self.uritemplate ):
@@ -198,17 +223,17 @@ class ProcessDeps:
         from_uri = self.make_uri( src )
         to_file = self.make_destination_file_name( src, dst )
         if from_uri == '':
-            print( "Error: Unable to evaluate source of:", src )
+            self.error( "Unable to evaluate source of: " + src )
             return
         if to_file == '':
-            print( "Error: Unable to evaluate destination of:", dst )
+            self.error( "Unable to evaluate destination of: " + dst )
             return
         if re.match( 'https?://', from_uri ):
             tmp_name = handler.download_to_temp_file( from_uri )
         else:
             tmp_name = self.local_copy_to_temp_file( from_uri )     # Taking a local copy is not optimal, but keeps the subsequent update logic the same
         if not tmp_name:
-            print( "Error: Unable to retrieve:", from_uri )
+            self.error( "Unable to retrieve: " + from_uri )
             return
         self.conditionally_update_dst_file( tmp_name, to_file )
 
@@ -217,9 +242,11 @@ class ProcessDeps:
             if os.path.dirname( to_file ):
                 os.makedirs( os.path.dirname( to_file ), exist_ok=True )
             shutil.move( tmp_name, to_file )
+            self.are_files_changed = ProcessDeps.are_any_files_changed = True
             print( 'Created...', to_file )
         elif not filecmp.cmp( tmp_name, to_file ):
             shutil.move( tmp_name, to_file )
+            self.are_files_changed = ProcessDeps.are_any_files_changed = True
             print( 'Updated...', to_file )
         else:
             os.unlink( tmp_name )
@@ -259,12 +286,12 @@ class ProcessDeps:
             elif var_name in self.vars:
                 uri = re.compile( '\$\{' + var_name + '\}' ).sub( self.vars[var_name], uri )
             else:
-                print( "Error: Unrecognised substitution variable:", var_name )
+                self.error( "Unrecognised substitution variable: " + var_name )
                 return ''
 
     def select_strand( self ):
         if 'strand' not in self.vars:
-            print( "Error: No suitable 'strand' variable available for substitution" )
+            self.error( "No suitable 'strand' variable available for substitution" )
             return ''
         strand = self.vars['strand']
         for supported_strands in self.versions:
@@ -276,7 +303,7 @@ class ProcessDeps:
         try:
             fout = None
             with open( file, 'rb' ) as fin:
-                fout = tempfile.NamedTemporaryFile( mode='wb', delete=False)
+                fout = tempfile.NamedTemporaryFile( mode='wb', delete=False )
                 while True:
                     data = fin.read( 1000 )
                     if not data:
@@ -286,6 +313,136 @@ class ProcessDeps:
             return fout.name
         except FileNotFoundError:
             return ''
+
+    def consider_subst( self, line ):
+        m = re.match( 'subst\s+(\S+)(?:\s+(\S+))?', line )
+        if m != None:
+            src = m.group(1)
+            dst = m.group(2)
+            if dst == None:
+                dst = src
+            try:
+                fout = None
+                with open( src, 'rt' ) as fin:
+                    fout = tempfile.NamedTemporaryFile( mode='wt', delete=False )
+                    for line in fin:
+                        fout.write( self.subst_expand_variables( line ) )
+                fout.close()
+                self.conditionally_update_dst_file( fout.name, dst )
+            except FileNotFoundError:
+                error( "Unable to open file for 'subst' command: " + src )
+            return True
+        return False
+
+    def subst_expand_variables( self, line ):
+        while( True ):
+            m = re.search( '\$\{exodep:(\w+)\}', line )
+            if m == None:
+                return line
+            var_name = m.group(1)
+            if var_name == 'strand':        # The strand variable has 'magic' properties (it can be mutated by a versions file) so we need to do something special with it
+                line = re.compile( '\$\{strand\}' ).sub( self.select_strand(), line )
+            elif var_name in self.vars:
+                line = re.compile( '\$\{exodep:' + var_name + '\}' ).sub( self.vars[var_name], line )
+            else:
+                self.error( "Unrecognised variable in 'subst' command: " + var_name )
+                return line
+
+    def consider_file_ops( self, line ):
+        m = re.match( '(cp|mv)\s+(\S+)\s+(\S+)', line )
+        if m != None:
+            op = m.group(1)
+            src = self.expand_variables( m.group(2) )
+            dst = self.expand_variables( m.group(3) )
+            if op == 'cp':
+                try:
+                    shutil.copy( src, dst )
+                except:
+                    self.error( "Unable to 'cp' file '" + src + "' to '" + dst + "'" )
+            elif op == 'mv':
+                try:
+                    shutil.move( src, dst )
+                except:
+                    self.error( "Unable to 'mv' file '" + src + "' to '" + dst + "'" )
+            return True
+        m = re.match( '(mkdir|rmdir|rm)\s+(\S+)', line )
+        if m != None:
+            op = m.group(1)
+            path = self.expand_variables( m.group(2) )
+            if op == 'mkdir':
+                try:
+                    os.makedirs( path, exist_ok=True )
+                except:
+                    self.error( "Unable to 'mkdir' for '" + path + "'" )
+            elif op == 'rmdir':
+                try:
+                    shutil.rmtree( path )
+                except:
+                    self.error( "Unable to 'rmdir' on '" + path + "'" )
+            elif op == 'rm':
+                try:
+                    os.unlink( path )
+                except:
+                    self.error( "Unable to 'rm' file '" + path + "'" )
+            return True
+        return False
+
+    def consider_exec( self, line ):
+        m = re.match( 'exec\s+(.+)', line )
+        if m != None:
+            command = m.group(1)
+            org_cwd = os.getcwd()
+            file_dirname = os.path.dirname( self.file )
+            if file_dirname:
+                os.chdir( file_dirname )
+            os.system( self.expand_variables( command ) )
+            os.chdir( org_cwd )
+            return True
+        return False
+
+    def consider_on_conditional( self, line ):
+        m = re.match( 'on\s+\$(\w+)\s+(.+)', line )
+        if m != None:
+            var_name = m.group(1)
+            command = m.group(2)
+            if var_name in self.vars and self.vars[var_name] != '':
+                self.process_line( command )
+            return True
+        return False
+
+    def consider_onchanged( self, line ):
+        m = re.match( 'onchanged\s+(.+)', line )
+        if m != None:
+            command = m.group(1)
+            if self.are_files_changed:
+                self.process_line( command )
+            return True
+        return False
+
+    def consider_onanychanged( self, line ):
+        m = re.match( 'onanychanged\s+(.+)', line )
+        if m != None:
+            command = m.group(1)
+            if ProcessDeps.are_any_files_changed:
+                self.process_line( command )
+            return True
+        return False
+
+    os_names = { 'windows': 'win32', 'linux': 'linux', 'osx': 'darwin' }
+
+    def consider_os_conditional( self, line ):
+        m = re.match( '(windows|linux|osx)\s+(.+)', line )
+        if m != None:
+            os_key = m.group(1)
+            command = m.group(2)
+            if sys.platform.startswith( ProcessDeps.os_names[os_key] ):
+                self.process_line( command )
+            return True
+        return False
+
+    def error( self, what ):
+        print( "Error:", self.file + ", line " + str(self.line_num) + ":" )
+        print( "      ", what )
 
 def remove_comments( line ):
     return re.compile( '\s*#.*' ).sub( '', line )
@@ -298,7 +455,7 @@ class TextDownloadHandler:
         try:
             fout = None
             with urllib.request.urlopen( uri ) as fin:
-                fout = tempfile.NamedTemporaryFile( mode='wt', delete=False)
+                fout = tempfile.NamedTemporaryFile( mode='wt', delete=False )
                 for line in fin:
                     fout.write( self.normalise_line_ending( line.decode('utf-8') ) )
             fout.close()
@@ -318,7 +475,7 @@ class BinaryDownloadHandler:
         try:
             fout = None
             with urllib.request.urlopen( uri ) as fin:
-                fout = tempfile.NamedTemporaryFile( mode='wb', delete=False)
+                fout = tempfile.NamedTemporaryFile( mode='wb', delete=False )
                 while True:
                     data = fin.read( 1000 )
                     if not data:
