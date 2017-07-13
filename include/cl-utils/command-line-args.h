94721e38f7fd66c8fe18168a67e6def415b5de19
Pete Cordell <pete.git@codalogic.com>
2017-07-13 18:32:30 +0100

    Added preliminary command-line argument parsing

---
+++ include/cl-utils/command-line-args.h
@@ -0,0 +1,91 @@
+//----------------------------------------------------------------------------
+// Copyright (c) 2017, Codalogic Ltd (http://www.codalogic.com)
+// All rights reserved.
+//
+// The license for this file is based on the BSD-3-Clause license
+// (http://www.opensource.org/licenses/BSD-3-Clause).
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// - Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// - Neither the name Codalogic Ltd nor the names of its contributors may be
+//   used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+// CommandLineArgs is a cheap and cheerful utility class to help make
+// processing command line arguments passed to main() easier.
+//----------------------------------------------------------------------------
+
+#ifndef CL_UTILS__COMMAND_LINE_ARGS
+#define CL_UTILS__COMMAND_LINE_ARGS
+
+#include <cstring>
+#include <iostream>
+
+namespace clutils {
+
+class CommandLineArgs
+{
+private:
+    int argc;
+    char ** argv;
+
+public:
+    CommandLineArgs( int argc, char ** argv ) : argc( argc ), argv( argv )
+    {
+        next();
+    }
+
+    bool is_flag()      // Is a flag as opposed to a value etc
+    {
+        return **argv == '-';
+    }
+    bool is_flag( const char * p_option_1, int desired_extra_count = 0 )  // Is specified flag
+    {
+        return (argc - 1) >= desired_extra_count && strcmp( p_option_1, *argv ) == 0;
+    }
+    bool is_flag( const char * p_option_1, const char * p_option_2, int desired_extra_count = 0 )
+    {
+        return (argc - 1) >= desired_extra_count && (strcmp( p_option_1, *argv ) == 0 || strcmp( p_option_2, *argv ) == 0);
+    }
+    bool ensure( int desired_extra_count, const char * p_on_insufficient_message )
+    {
+        if( (argc - 1) < desired_extra_count )
+        {
+            std::cout << p_on_insufficient_message << "\n";
+            argc = 0;   // Stop trying to process remaining insufficient set of arguments
+            return false;
+        }
+        return true;
+    }
+    const char * current() { return *argv; }
+    const char * next() { if( argc > 0 ) { ++argv; --argc; } return *argv; }
+    void operator ++ () { next(); }
+    void operator ++ (int) { next(); }
+    bool empty() const { return argc <= 0; }
+    operator bool () { return ! empty(); }
+};
+
+} // namespace clutils
+
+#endif // CL_UTILS__COMMAND_LINE_ARGS
--- main/main.cpp
+++ main/main.cpp
@@ -8,6 +8,61 @@
 
 #include "cl-jcr-parser/all.h"
 
-int main()
+#include "cl-utils/command-line-args.h"
+
+#include <iostream>
+
+void help()
 {
+    std::cout <<
+            "             cljcrparser - Codalogic JCR Parser\n"
+            "\n"
+            "Usage:\n"
+            "    cljcrparser [ flags ] <jcr-file-list>\n"
+            "\n"
+            "Flags:\n"
+            "    -h\n"
+            "    -? : Print this help information\n"
+            "    -json <file> : Specify JSON file to be validated against specified\n"
+            "                   JCR files\n"
+            "\n"
+            "<jcr-file-list> - One or more JCR files to verify.\n"
+            ;
+}
+
+
+bool capture_command_line( cljcr::Config * p_config, int argc, char ** argv )
+{
+    clutils::CommandLineArgs cla( argc, argv );
+
+    if( ! cla )
+    {
+        help();
+        return false;
+    }
+
+    for( ; cla; ++cla )
+    {
+        if( cla.is_flag( "-h", "-?" ) )
+        {
+            help();
+            return false;
+        }
+        else if( cla.is_flag( "-json" ) && cla.ensure( 1, "-json flag must include name of JSON file to validate" ) )
+            p_config->set_json( cla.next() );
+        else if( cla.is_flag() )
+            std::cout << "Unknown flag: " << cla.current() << "\n";
+        else
+            p_config->add_jcr( cla.current() );
+    }
+    
+    return true;
+}
+
+int main( int argc, char * argv[] )
+{
+    cljcr::Config config;
+    if( ! capture_command_line( &config, argc, argv ) )
+        return -1;
+    // return process_config( config );
 }
