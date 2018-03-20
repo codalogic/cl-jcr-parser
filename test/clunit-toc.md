d3a72a6fbb6aca742865cdab1e7c074b0283ea79
Pete Cordell <pete.git@codalogic.com>
2018-03-20 09:43:48 +0000

    Identified and recorded issues with UTF-8 chars in comments and error_token()

--- test/clunit-toc.md
+++ test/clunit-toc.md
@@ -1,4 +1,4 @@
-Tests table of contents generated on Mon Mar 19 10:39:12 2018
+Tests table of contents generated on Tue Mar 20 09:42:55 2018
 
 # test-config.cpp
 | Description | Line |
--- test/test-main.cpp
+++ test/test-main.cpp
@@ -37,4 +37,6 @@
 TFEATURE( "Miscellaneous TODOs" )
 {
     TTODO( "Investigate changes needed to remove requirement for g++ -Wno-parentheses flag" );
+    TTODO( "Make sure UTF-8 characters in comments do not cause problems" );
+    TTODO( "Modify GrammarParser::error_token() so it will grab whole UTF-8 characters" );
 }
